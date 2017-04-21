e27a0749e62d1420e3337a1408931007402d537e
abby sassel <sassela@live.com>
2017-04-21 06:29:30 +0100

    Use Bassbull library

---
+++ src/Bassbull.hs
@@ -0,0 +1,21 @@
+module Bassbull where
+
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Foldable as F
+import Data.Csv.Streaming
+
+type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
+
+fourth :: (a, b, c, d) -> d
+fourth (_, _, _, d) = d
+
+baseballStats :: BL.ByteString -> Either String (V.Vector BaseballStats)
+baseballStats -> decode NoHeader
+
+summer :: (a, b, c, Int) -> Int -> Int
+summer = (+) . fourth
+
+getAtBatsSum :: FilePath -> IO Int
+getAtBatsSum battingCsv = do
+  csvData <- BL.readFile battingCsv
+  return $ F.foldr summer 0 (baseballStats csvData)
--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,8 @@
 module Main where
 
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.Foldable as F
-import Data.Csv.Streaming
-
-type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
-
-fourth :: (a, b, c, d) -> d
-fourth (_, _, _, d) = d
-
-baseballStats :: BL.ByteString -> Either String (V.Vector BaseballStats)
-baseballStats -> decode NoHeader
+import Bassbull
 
 main :: IO ()
 main = do
-  csvData <- BL.readFile "batting.csv"
-  let summed = F.foldr summer 0 (baseballStats csvData)
+  summed <- getAtBatsSum "batting.csv"
   putStrLn $ "Total atBats was: " ++ (show summed)
-  where summer = (+) . fourth
